f91d0adef17b3142ea6a4d1def0ccb70e55de2b7
franciscocaloia <59297230+franciscocaloia@users.noreply.github.com>
2023-03-29 15:12:45 -0300

    redirect

--- taxi-moto/src/components/cadete/orders/negoicoOrderList.jsx
+++ taxi-moto/src/components/cadete/orders/negoicoOrderList.jsx
@@ -3,16 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { NegocioOrderListItem } from "./negocioOrderListItem";
 
 export const NegocioOrderList = ({ orders }) => {
-  const navigate = useNavigate();
   return (
     <>
-      <button
-        onClick={() => {
-          navigate(0);
-        }}
-      >
-        refresh
-      </button>
       <ul className="w-5/6 mx-auto lg:grid lg:grid-cols-2 2xl:grid-cols-3 lg:w-4/5">
         {orders?.map((order) => (
           <NegocioOrderListItem key={order._id} order={order} />
