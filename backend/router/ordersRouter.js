b1ac230bab8596212f7bf58b7951abf27babf421
Francisco Caloia <franciscocaloia@hotmail.com>
2025-03-17 09:28:59 -0300

    Update ordersRouter.js

--- backend/router/ordersRouter.js
+++ backend/router/ordersRouter.js
@@ -21,7 +21,7 @@ export const ordersRouter = Router();
 ordersRouter.get("/prices", (req, res, next) => {
   return res.status(200).json({
     0: {
-      shipment: 1100,
+      shipment: 1250,
       additional: 50,
     },
     1000: {
